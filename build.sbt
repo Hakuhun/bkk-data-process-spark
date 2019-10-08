1dd5e4d48f91c22bb8f0d6b18d1b4abe3794dc16
bakonyigergo <bakonyi_gergo@4dsoft.hu>
2019-10-08 15:55:44 +0200

    Project generation

--- build.sbt
+++ build.sbt
@@ -4,5 +4,5 @@ version := "0.1"
 
 scalaVersion := "2.12.10"
 
-// https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.4"
--- src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
+++ src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
@@ -1,13 +1,42 @@
 package hu.oe.bakonyi.bkk
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.kafka.common.serialization.Deserializer
+
+//https://spark.apache.org/docs/latest/streaming-kafka-0-10-integration.html
+//https://spark.apache.org/docs/latest/streaming-programming-guide.html
 object BkkDataProcesser {
 
-  def add(x : Int, y : Int): Int ={
-    return x + y;
-  }
+  val conf = new SparkConf().setMaster("local[2]").setAppName("bkk-process")
+  val ssc = new StreamingContext(conf, Seconds(1))
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "use_a_separate_group_id_for_each_stream",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+
+  val topics = Array("bkk")
+
+  val stream = KafkaUtils.createDirectStream(
+    ssc,
+    PreferConsistent,
+    Subscribe(topics, kafkaParams)
+  )
+
+  stream.map(record => (record.key, record.value))
 
   def main(args: Array[String]): Unit = {
-    print(s"Hello világ ${add(1,2)}")
+
   }
 
 

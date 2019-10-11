00356f9049f4f75ea229e9215e4ba997ba08a98c
bakonyigergo <bakonyi_gergo@4dsoft.hu>
2019-10-11 12:12:59 +0200

    Project generation

--- src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
+++ src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
@@ -1,7 +1,9 @@
 package hu.oe.bakonyi.bkk
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010._
@@ -9,12 +11,13 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 //https://spark.apache.org/docs/latest/streaming-kafka-0-10-integration.html
 //https://spark.apache.org/docs/latest/streaming-programming-guide.html
+//https://stackoverflow.com/questions/36421619/whats-the-meaning-of-dstream-foreachrdd-function
 object BkkDataProcesser {
 
-  val conf = new SparkConf().setMaster("local[2]").setAppName("bkk-process")
-  val ssc = new StreamingContext(conf, Seconds(1))
+  val conf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("bkk-process")
+  val ssc: StreamingContext = new StreamingContext(conf, Seconds(1))
 
-  val kafkaParams = Map[String, Object](
+  val kafkaParams: Map[String, Object] = Map[String, Object](
     "bootstrap.servers" -> "localhost:9092",
     "key.deserializer" -> classOf[StringDeserializer],
     "value.deserializer" -> classOf[StringDeserializer],
@@ -23,17 +26,19 @@ object BkkDataProcesser {
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
 
+  val topics: Array[String] = Array("bkk")
 
-
-  val topics = Array("bkk")
-
-  val stream = KafkaUtils.createDirectStream(
+  val stream: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream(
     ssc,
     PreferConsistent,
     Subscribe[String,String](topics, kafkaParams)
   )
 
-  stream.map(record => (record.key, record.value))
+  val records: DStream[(String, String)] = stream.map(record => (record.key, record.value))
+
+  stream.foreachRDD{rdd=>
+    rdd.first().value()
+  }
 
   stream.foreachRDD { rdd =>
     val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
@@ -41,10 +46,9 @@ object BkkDataProcesser {
     stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
   }
 
-
   def main(args: Array[String]): Unit = {
     print("ASD"+System.lineSeparator())
-    ssc.start();
+    ssc.start()
     ssc.awaitTermination()
   }
 

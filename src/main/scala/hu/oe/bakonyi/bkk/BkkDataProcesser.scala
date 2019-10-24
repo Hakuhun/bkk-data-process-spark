c6d08cf4e59996a6b1881a8f46246a5d37d68b73
bakonyig <bakonyi.gergo.istvan@gmail.com>
2019-10-25 00:04:20 +0200

    Costume deserializator for BkkBusinessData

--- src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
+++ src/main/scala/hu/oe/bakonyi/bkk/BkkDataProcesser.scala
@@ -1,9 +1,11 @@
 package hu.oe.bakonyi.bkk
 
-import hu.oe.bakonyi.bkk.model.BkkBusinessDataV2
+import hu.oe.bakonyi.bkk.model.{BkkBusinessDataV2, BkkBusinessDataV3}
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.sql.catalyst.expressions.GenericRow
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
@@ -13,12 +15,19 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 //https://spark.apache.org/docs/latest/streaming-kafka-0-10-integration.html
 //https://spark.apache.org/docs/latest/streaming-programming-guide.html
 object BkkDataProcesser {
-
   val log : Logger = Logger.getLogger(BkkDataProcesser.getClass)
 
   val conf = new SparkConf().setMaster("local[2]").setAppName("bkk-process")
   val ssc = new StreamingContext(conf, Seconds(1))
   ssc.sparkContext.setLogLevel("ERROR")
+
+  val modelDirectory = "/mnt/D834B3AF34B38ECE/DEV/hadoop/"
+
+  val lr = new LogisticRegression()
+    .setMaxIter(100)
+    .setRegParam(0.02)
+    .setElasticNetParam(0.3)
+
 
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "localhost:9092",
@@ -47,13 +56,38 @@ object BkkDataProcesser {
 
         val bkkData: Dataset[BkkBusinessDataV2] = sparkSession.createDataset(rdd)
         printf(s"Egy RDD groupby után: ${System.lineSeparator()}")
-        //val num = bkkData.groupByKey(
-        //  x => (x.month, x.dayOfWeek, x.lastUpdateTime, x.routeId, x.tripId)
-        //)
 
         val aggregatedAvgData = bkkData.groupBy($"month",$"dayOfWeek",$"lastUpdateTime", $"routeId", $"tripId", $"stopId").avg()
-        //val bkkv3Data = aggregatedAvgData.map()
-        
+        aggregatedAvgData.show(10, false)
+        val bkkv3Data: Dataset[BkkBusinessDataV3] = aggregatedAvgData
+          .map {
+            case row: GenericRow => BkkBusinessDataV3(
+              row.getAs[Int]("month"),
+              row.getAs[Int]("dayOfWeek"),
+              row.getAs("routeId"),
+              row.getAs[Double]("avg(temperature)"),
+              row.getAs[Double]("avg(humidity)"),
+              row.getAs[Double]("avg(pressure)"),
+              row.getAs[Double]("avg(snow)"),
+              row.getAs[Double]("avg(rain)"),
+              row.getAs[Double]("avg(visibility)"),
+              ((row.getAs[Double]("avg(departureDiff)") + row.getAs[Double]("avg(arrivalDiff)")) / 2)
+            )
+          }
+        bkkv3Data.show(10, false)
+
+        try {
+          val smodel = org.apache.spark.ml.PipelineModel.load(modelDirectory)
+          smodel.transform(bkkv3Data).write.save(modelDirectory)
+        }catch {
+          case ex : UnsupportedOperationException =>{
+            val model = lr.fit(bkkv3Data)
+            model.write.overwrite().save(modelDirectory)
+          }
+        }
+
+
+
 
       }
     }
